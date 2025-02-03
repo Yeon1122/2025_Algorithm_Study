5eb50d89d6048307a105597c424fd837540d2a2a
chandel <seyoon.park@alumni.kaist.ac.kr>
2025-02-04 01:10:45 +0900

    [+] add problem 1~4 [-] remove temp

---
+++ P/1_week/problem03.py
@@ -0,0 +1,33 @@
+############## 주의 ##############
+# 입력을 받기위한 input 함수는 절대 사용하지 않습니다.
+def is_user_data_valid(user_data):
+    # id에 'id' 값 넣고 password에 'password' 값 넣기
+    id = user_data.get('id')
+    password = user_data.get('password')
+    # id와 password 모두 True일 시 True 반환
+    if id and password:
+        return True
+    else:
+        return False
+    
+
+# 추가 테스트를 위한 코드 작성 가능
+# 예) print(함수명(인자))
+
+#####################################################
+# 아래 코드를 삭제하는 경우 
+# 모든 책임은 삭제한 본인에게 있습니다. 
+############## 테스트 코드 삭제 금지 #################
+user_data1 = {
+    'id': '',
+    'password': '1q2w3e4r',
+}
+print(is_user_data_valid(user_data1)) # False 
+
+
+user_data2 = {
+    'id': 'jungssafy',
+    'password': '1q2w3e4r',
+}
+print(is_user_data_valid(user_data2)) # True
+#####################################################
