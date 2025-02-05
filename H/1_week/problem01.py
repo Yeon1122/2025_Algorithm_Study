da95b3acf0f2b6e8042db165c3ecf985ab243349
jayee <lucete10167@naver.com>
2025-02-05 20:53:38 +0900

    Add files via upload

--- H/1_week/problem01.py
+++ H/1_week/problem01.py
@@ -1,26 +1,39 @@
 ############## 주의 ##############
-# 입력을 받기위한 input 함수는 절대 사용하지 않습니다.
-# 파이썬 내장 함수 min 함수를 사용하지 않습니다.
+# 입력을 받기 위한 input 함수는 절대 사용하지 않습니다.
+# Python 내장함수 sum, len, min, max, sorted 또는 리스트 sort 메서드 사용 시 감점
 
-def min_score(scores):
-    min_value = scores[0]
-    for score in scores:
-        if score < min_value:
-            min_value = score
-    return min_value
+def analyze_likes(weekly_like_list):
+    sum_list = 0
+    size_list = 0
+    min_list = max_list = weekly_like_list[0]
 
+    for number in weekly_like_list:
+        size_list += 1
+        sum_list += number
+        
+        if number < min_list:
+            min_list = number
+        
+        if number > max_list:
+            max_list = number
+    
+    return (sum_list/size_list, max_list-min_list)
+
+    # 여기에 코드를 작성하여 함수를 완성합니다.
 
 # 추가 테스트를 위한 코드 작성 가능
 # 예) print(함수명(인자))
 
 #####################################################
-# 아래 코드를 삭제하는 경우 
-# 모든 책임은 삭제한 본인에게 있습니다. 
+# 아래 코드를 삭제하는 경우
+# 모든 책임은 삭제한 본인에게 있습니다.
 ############## 테스트 코드 삭제 금지 #################
-print(min_score([30, 60, 90, 70])) # 30
-print(min_score([0, 10, 20, 30, 40, 50])) # 0
-print(min_score([50, 70, 50, 45, 80, 80])) # 45
-#####################################################
+analyze_likes([2, 5, 3, 8, 0, 10, 4])
+# 1) 평균 = (2 + 5 + 3 + 8 + 0 + 10 + 4) / 7
+#    = 32 / 7 ≈ 4.5714...
+# 2) 최소=0, 최대=10, 차이=10
+# 결과: (4.5714..., 10)
+print(analyze_likes([2, 5, 3, 8, 0, 10, 4]))  # 예시: (4.5714..., 10)
+print(analyze_likes([7, 7, 7, 7, 7, 7, 7]))   # (7.0, 0)
 
-# 테스트 코드는 이곳에
-print(min_score([100, 100]))
+#####################################################
