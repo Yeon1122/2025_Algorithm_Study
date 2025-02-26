b4a8b9bd670eb1973ce3400049af80cdf7d2140a
lhy <lhy9822@gmail.com>
2025-02-26 11:18:33 +0900

    [+] Baekjoon_2789

---
+++ L/4_week/Baekjoon_2798_블랙잭.py
@@ -0,0 +1,13 @@
+N, M = map(int,input().split())
+cards = list(map(int,input().split()))
+answer = 0
+
+for i in range(N):
+    for j in range(i+1,N):
+        for k in range(j+1,N):
+            if cards[i] + cards[j] + cards[k] <= M:
+                answer = max(answer,cards[i] + cards[j] + cards[k])
+
+print(answer)
+
+#6분 30초 정답
