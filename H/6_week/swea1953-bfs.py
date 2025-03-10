09276d21331b5d8479d563fff09f0f2e42826d09
jayee <lucete10167@naver.com>
2025-03-10 17:54:24 +0900

    다풀기!!

--- H/6_week/swea1953-bfs.py
+++ H/6_week/swea1953-bfs.py
@@ -1,70 +1,47 @@
-import sys
-sys.stdin = open("input.txt", "r")
 from collections import deque
 
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
-types = {
-    1: [1, 1, 1, 1],
-    2: [1, 1, 0, 0],
-    3: [0, 0, 1, 1],
-    4: [1, 0, 0, 1],
-    5: [0, 1, 0, 1],
-    6: [0, 1, 1, 0],
-    7: [1, 0, 1, 0]
-}
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]          # 상하좌우 이동
+tunnels = [
+    0,
+    [0, 1, 2, 3], 
+    [0, 1],
+    [2, 3],
+    [0, 3],
+    [1, 3],
+    [1, 2],
+    [0, 2]
+]
 
 def bfs(R, C):
-    dq = deque([(R, C)])    
-    visited[R][C] = 1 
+    q = deque([R, C])
+    visited[R][C] = 1
 
-    while dq:
-        nowy, nowx = dq.popleft()
-        dirs = types[graph[nowy][nowx]]
+    while q:
+        r, c = q.popleft()
+        dir = tunnels[tunnels[r][c]]
+        for i in range(4):
+            if dir[i] == 0:
+                continue
+            next_r, next_c = r + dr[i], c + dc[i]
 
-        for i in range(4):      
-            if dirs[i] == 0:
+            if 0 > next_r or next_r >= N or 0 > next_c or next_c >= M:      # 가능범위가 0 <= r < N 이니까 그 반대
+                continue
+            if visited[next_r][next_c]: # 0이 아니면 이미 지나간거니까 패스스
+                continue
+            if tunnels[next_r][next_c] == 0:    # 이동불가가
                 continue
 
-            new_y = nowy + dy[i]
-            new_x = nowx + dx[i]
+                
 
-            if new_y < 0 or new_y >= N or new_x < 0 or new_x >= M:
-                continue
 
-            if visited[new_y][new_x]:
-                continue
-
-            if graph[new_y][new_x] == 0:
-                continue
-
-            next_dirs = types[graph[new_y][new_x]]
-
-            if i % 2 == 0 and next_dirs[i + 1] == 0:
-                continue
-
-            if i % 2 == 1 and next_dirs[i - 1] == 0:
-                continue
-
-            if visited[nowy][nowx] + 1 > L:
-                continue
-
-            visited[new_y][new_x] = visited[nowy][nowx] + 1
-            dq.append((new_y, new_x))
 
 T = int(input())
-
-for tc in range(1, T + 1):
+for tc in range(1, T+1):
+    result = 0
     N, M, R, C, L = map(int, input().split())
-    graph = [list(map(int, input().split())) for _ in range(N)]
-    visited = [[0] * M for _ in range(N)]
-
+    origin = [[list(map(int, input().split()))] for _ in range(N)]
+    visited = [[0] * M] for _ in range(N)
     bfs(R, C)
 
-    cnt = 0
-    for i in range(N):
-        for j in range(M):
-            if 0 < visited[i][j] <= L:
-                cnt += 1
-
-    print(f'#{tc} {cnt}')
+    print(f"{tc} {result}")
