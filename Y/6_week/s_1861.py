88c2c8aa89ae5d700a556ce28101d4c814ee580e
yang-yang140 <yhyji.ssafy@gmail.com>
2025-03-16 23:19:17 +0900

    s 1861 정사각형 방

--- Y/6_week/s_1861.py
+++ Y/6_week/s_1861.py
@@ -5,42 +5,42 @@
 #   한 번 마지막 지점에 갔으면 max_cnt 갱신
 #   #
 T = int(input())
-#delta
+# delta
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
-for test_case in range(1, T+1):
+for test_case in range(1, T + 1):
     N = int(input())
     graph = [list(map(int, input().split())) for _ in range(N)]
     max_cnt = float('-inf')
-    idx = []
+    cnt = float('-inf')
 
 
     def dfs(cur_r, cur_c, cnt):
-        global max_cnt, idx
-        visited[cur_r][cur_c] = 1
+        global max_cnt, cnts
+        # visited[cur_r][cur_c] = 1
+        # cnt += 1
+        cnts = cnt
 
         for d in range(4):
-            nr = cur_r+dr[d]
-            nc = cur_c+dc[d]
+            nr = cur_r + dr[d]
+            nc = cur_c + dc[d]
 
-            if 0<=nr<N and 0<=nc<N and visited[nr][nc] == 0:
-                if graph[nr][nc]-1 == graph[cur_r][cur_c] or graph[nr][nc]+1 == graph[cur_r][cur_c]:
-                    dfs(nr, nc, cnt+1)
+            if 0 <= nr < N and 0 <= nc < N and graph[nr][nc] == graph[cur_r][cur_c] + 1:
+                dfs(nr, nc, cnt + 1)
 
-        if cnt>max_cnt:
-            if len(idx):
-                if graph[idx[0]][idx[1]]>graph[cur_r][cur_c]:
-                    idx = [cur_r, cur_c]
-                    max_cnt = cnt
-            else:
-                idx = [cur_r, cur_c]
-                max_cnt = cnt
-
-        return cnt
 
     for i in range(N):
         for j in range(N):
-            visited = [[0] * N for _ in range(N)]
-            dfs(i, j, 0)
+            # cnt = 0
+            idx_num = graph[i][j]
+            dfs(i, j, 1)
 
-    print(f'#{test_case} {max_cnt} {graph[idx[0]][idx[1]]}')
+            if max_cnt < cnts:
+                max_cnt = cnts
+                idx = [i, j]
+
+            elif max_cnt == cnts:
+                if graph[i][j] < graph[idx[0]][idx[1]]:
+                    idx = [i, j]
+
+    print(f'#{test_case} {graph[idx[0]][idx[1]]} {max_cnt}')
